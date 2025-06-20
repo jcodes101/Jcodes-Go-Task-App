259671989c5f55ced60cd786f314d393b1dd556e
jadin <jshutchinson@aggies.ncat.edu>
2025-06-20 13:23:08 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("|-| Welcome to Jcodes To-Do List App! |-|")
+	fmt.Println("=========================================")
+	fmt.Println()
+
+	fmt.Println("List of Tasks:")
+
+	fmt.Println("Animes to watch:")
+	fmt.Println("Watch Solo Leveling")
+	fmt.Println("Watch Fairytale")
+	fmt.Println("Watch One Piece")
+	fmt.Println()
+
+	fmt.Println("Programming Languages to learn:")
+	fmt.Println("Learn Go")
+	fmt.Println("Learn Typescript")
+	fmt.Println("Learn Swift")
+	fmt.Println()
+
+}
